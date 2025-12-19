d8685ced4eda38cc238e90bb30393adc52e92e04
xm2x <Mohmmed200621@gmail.com>
2025-12-19 23:30:53 +0200

    ..

---
+++ correct code/bookstore/log-in form/login.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS bookstore_db;
+
+USE bookstore_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
